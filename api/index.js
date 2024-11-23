9186b932f45bc9d70ceedabdd161f5f3e986f328
Paresh-95 <Pareshbhamare95@gmail.com>
2024-11-23 20:58:26 +0530

    added link

--- api/index.js
+++ api/index.js
@@ -11,11 +11,7 @@ const PORT = process.env.PORT || 4000
 
 app.use(
     cors({
-      origin: [
-        "http://localhost:3000",
-        "https://blog-app-backend-attack-capital.vercel.app"
-        
-      ],
+      origin:"https://blog-app-frontend-attack-capital.vercel.app",
       credentials: true,
     })
   );
