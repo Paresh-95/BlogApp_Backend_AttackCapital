8b751b22876710e2074dad3ba1bd389d3b1efc58
Paresh-95 <Pareshbhamare95@gmail.com>
2024-11-23 21:30:39 +0530

    added cookies options

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -99,6 +99,8 @@ export const login = async (req, res) => {
     const options = {
       expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
       httpOnly: true,
+      secure: true, 
+      sameSite: "none", 
     };
 
     res.cookie("token", token, options).status(200).json({
