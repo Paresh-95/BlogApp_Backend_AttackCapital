bd4e23dc9cd0c531bbc1bf5691603e0a6477977c
Paresh-95 <Pareshbhamare95@gmail.com>
2024-11-24 14:39:30 +0530

    cors changes

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 4000
 
 app.use(
     cors({
-      origin:"https://blog-app-frontend-attack-capital.vercel.app/",
+      origin:"https://blog-app-frontend-attack-capital.vercel.app",
       // origin:"http://localhost:3000",
       credentials: true,
     })
