f8dd3b81fe50ee71a34130863d2a0f9a77a9611b
Paresh-95 <Pareshbhamare95@gmail.com>
2024-11-27 15:09:54 +0530

    added

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -48,8 +48,8 @@ export const signup = async (req, res) => {
       expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
       httpOnly: true,
       secure: true, 
-      sameSite: "lax", 
-      path:"/"
+      sameSite: "none", 
+      path:'/'
     };
     
     res.cookie("token", token, options);
